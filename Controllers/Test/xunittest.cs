c9930f0a1dcdd23195e3c421a345598cd6a793ee
binyamz <binirtis@gmail.com>
2016-04-17 09:29:53 -0500

    more test

--- Controllers/Test/xunittest.cs
+++ Controllers/Test/xunittest.cs
@@ -9,8 +9,16 @@ namespace BasicAspNet.Test
                 {
                 Assert.Equal(4, Add(2, 2));
                 }
-        
-                
+                public void PassingTest2()
+                {
+                Assert.Equal(4, Add(2, 2));
+                }
+                [Fact]
+                public void FailingTest()
+                {
+                Assert.Equal(5, Add(2, 2));
+                }
+               
         
                 int Add(int x, int y)
                 {
