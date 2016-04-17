f7d70e2da7857c62c287424b897dc7eba19e7e49
binyamz <binirtis@gmail.com>
2016-04-16 20:08:08 -0500

    pass all test

--- Controllers/Test/xunittest.cs
+++ Controllers/Test/xunittest.cs
@@ -10,11 +10,7 @@ namespace BasicAspNet.Test
                 Assert.Equal(4, Add(2, 2));
                 }
         
-                [Fact]
-                public void FailingTest()
-                {
-                Assert.Equal(5, Add(2, 2));
-                }
+                
         
                 int Add(int x, int y)
                 {
